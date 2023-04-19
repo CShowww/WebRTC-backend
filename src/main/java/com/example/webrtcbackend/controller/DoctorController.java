6a53a1c27e80cf7accc5b966ddb767881f1bdc37
Cangjian <cangjian.gao@student.unimelb.edu.au>
2023-04-19 18:21:21 +1000

    Doctor service initialization

---
+++ src/main/java/com/example/webrtcbackend/controller/DoctorController.java
@@ -0,0 +1,4 @@
+package com.example.webrtcbackend.controller;
+
+public class DoctorController {
+}
---
+++ src/main/java/com/example/webrtcbackend/entity/Doctor.java
@@ -0,0 +1,26 @@
+package com.example.webrtcbackend.entity;
+
+
+import lombok.Data;
+import java.io.Serializable;
+
+@Data
+public class Doctor implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String username;
+
+    private String name;
+
+    private String password;
+
+    private String phone;
+
+    private String sex;
+
+    private String idNumber;
+
+    private Integer role;
+}
