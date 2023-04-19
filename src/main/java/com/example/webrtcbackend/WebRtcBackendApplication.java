77dac5a829ffdd2bcbb33157b07092d4e2868a92
Cangjian <cangjian.gao@student.unimelb.edu.au>
2023-04-19 17:41:28 +1000

    First commit

---
+++ src/main/java/com/example/webrtcbackend/WebRtcBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.webrtcbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRtcBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebRtcBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webrtcbackend/WebRtcBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webrtcbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebRtcBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
