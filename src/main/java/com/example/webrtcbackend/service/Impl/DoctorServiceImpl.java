1124727f8fc3e998e20d978ec527ff3cf7ccc22c
Jeremy-Shang <shangyuan109854496@qq.com>
2023-04-22 14:07:28 +1000

    update

--- src/main/java/com/example/webrtcbackend/service/Impl/DoctorServiceImpl.java
+++ src/main/java/com/example/webrtcbackend/service/Impl/DoctorServiceImpl.java
@@ -6,8 +6,8 @@ import com.example.webrtcbackend.mapper.DoctorMapper;
 import com.example.webrtcbackend.service.DoctorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-//
-//@Service
-//public class DoctorServiceImpl extends ServiceImpl<DoctorMapper, Doctor> implements DoctorService {
-//
-//}
+
+@Service
+public class DoctorServiceImpl extends ServiceImpl<DoctorMapper, Doctor> implements DoctorService {
+
+}
