abf02f9ad4c5857ff202e6029ff53b497f57da0a
Jeremy-Shang <shangyuan109854496@qq.com>
2023-04-23 16:04:10 +1000

    update

--- src/main/java/com/example/webrtcbackend/mapper/DoctorMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.webrtcbackend.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.webrtcbackend.entity.Doctor;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DoctorMapper extends BaseMapper<Doctor> {
-}
