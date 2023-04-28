896a0b3ff14525eba944e56177f6745ac2da7168
Jeremy-Shang <shangyuan109854496@qq.com>
2023-04-28 20:20:41 +1000

    upload ok

---
+++ src/main/java/com/example/webrtcbackend/controller/CommonController.java
@@ -0,0 +1,38 @@
+package com.example.webrtcbackend.controller;
+
+
+import com.example.webrtcbackend.common.R;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * 文件上传和下载
+ */
+@RestController
+@RequestMapping("/common")
+@Slf4j
+public class CommonController {
+
+
+    /**
+     * 文件上传
+     *
+     * @param file
+     * @return 文件上传的目录改为项目运行的根目录
+     */
+    @PostMapping("/upload")
+    public R<String> upload(MultipartFile file) {
+        //file是一个临时文件，需要转存到指定位置，否则本次请求完成后临时文件会删除
+        log.info(file.getOriginalFilename().toString());
+
+        //原始文件名
+        String originalFilename = file.getOriginalFilename();//abc.jpg
+
+        return R.success("any");
+    }
+
+
+}
--- src/main/java/com/example/webrtcbackend/controller/UserController.java
+++ src/main/java/com/example/webrtcbackend/controller/UserController.java
@@ -2,16 +2,21 @@ package com.example.webrtcbackend.controller;
 
 
 import com.example.webrtcbackend.common.R;
-import com.example.webrtcbackend.entity.bo.User;
-import com.example.webrtcbackend.service.FhirService;
 import com.example.webrtcbackend.service.UserService;
-import com.example.webrtcbackend.token.UserLoginToken;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.system.ApplicationHome;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Map;
+import java.io.File;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 @Slf4j
 @RestController
@@ -19,16 +24,87 @@ import java.util.Map;
 @CrossOrigin
 public class UserController {
 
-    // 用户服务
     @Autowired
     UserService userService;
 
+    @Value("${common.basepath}")
+    String basePath;
+
+    /**
+     * 登陆服务
+     * @param httpServletRequest
+     * @return
+     */
     @PostMapping("/login")
     public R<String> login(HttpServletRequest httpServletRequest) {
         log.info("Save Token");
+
         userService.saveToken(httpServletRequest);
+
+        String userId = httpServletRequest.getParameter("userId");
+
+        httpServletRequest.getSession().setAttribute("userId", userId);
 
         return R.success();
     }
+
+    /**
+     * 文件上传服务 pdf?
+     * @param file
+     * @return
+     */
+    @PostMapping("/upload")
+    public R<String> upload(MultipartFile file, HttpServletRequest request, @RequestParam("fileType") String fileType) {
+
+        log.info("User upload file " + file.getOriginalFilename().toString());
+
+        // rename by timestamp
+        String timestamp = LocalDateTime.now()
+                .format(DateTimeFormatter.ofPattern("_yyyy-MM-dd_HH:mm:ss"));
+
+        String fileNameSplit[] = file.getOriginalFilename()
+                .split("\\.");
+
+        String newName = fileNameSplit[0] + timestamp + "." + fileNameSplit[1];
+
+        // get running jar dir
+        ApplicationHome home = new ApplicationHome(getClass());
+
+        File targetPath = new File(home.getDir().getPath() + basePath + request.getParameter("userId") + "/" + fileType + "/");
+
+        log.info("jar running at path: " + targetPath.getPath());
+
+        if (!targetPath.exists()) {
+            targetPath.mkdirs();
+        }
+
+        // file transfer
+        try {
+            file.transferTo(new File(targetPath.getPath() + "/" + newName));
+        } catch (IOException e) {
+            e.printStackTrace();
+            log.error("upload error");
+
+            return R.error("upload error: file transfer fail");
+        }
+
+        return R.success(newName);
+
+    }
+
+    // TODO, 下载，返回最晚创建的文件
+    // TODO, 上传健康数据，先Append到csv文件中
+    // 然后直接下载
+    /**
+     * 文件下载服务
+     */
+    @GetMapping("/download")
+    public void download(HttpServletResponse response, @RequestParam("fileType") String fileType) {
+        log.info("hi");
+
+    }
+
+
+
 
 }
