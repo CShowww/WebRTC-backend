840d0aeb9c8dc127e8efddd3f2209d202a811507
Cangjian <cangjian.gao@student.unimelb.edu.au>
2023-05-22 22:55:14 +1000

    Send email logic

--- src/main/java/com/vd/backend/util/EmailSender.java
+++ src/main/java/com/vd/backend/util/EmailSender.java
@@ -1,16 +1,34 @@
 package com.vd.backend.util;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.vd.backend.entity.bo.Connector;
+import com.vd.backend.service.FhirService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
-import java.util.Properties;
 import javax.mail.*;
-import javax.mail.internet.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
+@Slf4j
 public class EmailSender {
-    Connector connector;
+    private Connector connector;
+    private Session session;
+    private Message message;
+    private String content;
+    private String[] recipient;
 
-    public void sendEmail(String recipient, String subject, String content) {
+    @Autowired
+    private FhirService fhirService;
+    // subject  = "测试邮件";
+    // content = "这是一封测试邮件。"
+    // recipient = "recipient@example.com"; // 接收方邮箱地址
+
+    public void init(){
         // 配置发送邮件的属性
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
@@ -26,32 +44,43 @@ public class EmailSender {
         };
 
         // 创建会话
-        Session session = Session.getInstance(props, auth);
+        session = Session.getInstance(props, auth);
+        message = new MimeMessage(session);
+        recipient = new String[2];// practitioner and patient
 
-        try {
-            // 创建邮件消息
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress("${email.username}"));
-            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
-            message.setSubject(subject);
-            message.setText(content);
-
-            // 发送邮件
-            Transport.send(message);
-
-            System.out.println("邮件发送成功！");
-        } catch (MessagingException e) {
-            System.out.println("邮件发送失败：" + e.getMessage());
-        }
     }
 
-    public void acceptData(JSONObject data){
+    public void sendEmail(JSONObject data) {
+        parseData(data);
+        log.info("Data from front end : {}",data);
+        init();
+
+
+        if(connector.getType().equals("Add")){
+            content = "One patient made an appointment!";
+            send2Patient(connector.getPatientId(),"Patient", content);
+            send2Practitioner(connector.getPractitionerId(), "Practitioner", content);
+        }
+        else if(connector.getType().equals("Update")){
+            content = "The patient updated the appointment.";
+            send2Practitioner(connector.getPractitionerId(), "Patient", content);
+        }
+        else{
+            content = "The practitioner cancelled the appointment.";
+            send2Patient(connector.getPatientId(), "Practitioner", content);
+        }
+
+
+    }
+
+    public void parseData(JSONObject data){
         String type = data.getString("type");
         connector.setType(type);
 
         if(type.equals("Update")){
             connector.setPatientId(data.getString("patientId"));
             connector.setPatientName(data.getString("patientName"));
+
         }
         else if(type.equals("Delete")){
             connector.setPractitionerId(data.getString("practitionerId"));
@@ -63,6 +92,53 @@ public class EmailSender {
             connector.setPractitionerId(data.getString("practitionerId"));
             connector.setPractitionerName(data.getString("practitionerName"));
         }
+
     }
 
+
+    @GetMapping("/fhir/profiles/{resource}/{id}")
+    public void send2Patient(@PathVariable String id, @PathVariable String resource, String content){
+        String rel = "";
+        rel = fhirService.get(resource, id);
+        JSONObject data = JSON.parseObject(rel);
+        recipient[0] = data.getString("email");
+
+        try {
+            // 创建邮件消息
+            message.setFrom(new InternetAddress("${email.username}"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient[0]));
+            message.setSubject("${email.subject}\"");
+            message.setText(content);
+
+            // 发送邮件
+            Transport.send(message);
+
+            System.out.println("邮件发送成功！");
+        } catch (MessagingException e) {
+            System.out.println("邮件发送失败：" + e.getMessage());
+        }
+    }
+
+    @GetMapping("/fhir/profiles/{resource}/{id}")
+    public void send2Practitioner(@PathVariable String id, @PathVariable String resource, String content){
+        String rel = "";
+        rel = fhirService.get("Practitioner", id);
+        JSONObject data = JSON.parseObject(rel);
+        recipient[1] = data.getString("email");
+
+        try {
+            // 创建邮件消息
+            message.setFrom(new InternetAddress("${email.username}"));
+            message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient[1]));
+            message.setSubject("${email.subject}\"");
+            message.setText(content);
+
+            // 发送邮件
+            Transport.send(message);
+
+            System.out.println("邮件发送成功！");
+        } catch (MessagingException e) {
+            System.out.println("邮件发送失败：" + e.getMessage());
+        }
+    }
 }
