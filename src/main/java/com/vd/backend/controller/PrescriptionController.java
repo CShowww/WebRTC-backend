87def9df23a1585e9dcf62484d1f51f0642b9ace
Cangjian <cangjian.gao@student.unimelb.edu.au>
2023-05-26 01:04:00 +1000

    prescription

--- src/main/java/com/vd/backend/controller/PrescriptionController.java
+++ src/main/java/com/vd/backend/controller/PrescriptionController.java
@@ -4,7 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 import com.vd.backend.common.R;
-import com.vd.backend.service.FhirService;
+import com.vd.backend.service.HttpFhirService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,7 +15,7 @@ import java.util.Date;
 @RequestMapping("/fhir")
 public class PrescriptionController {
 
-    FhirService fhirService;
+    HttpFhirService fhirService;
     @GetMapping("/MedicationDispense/{resource}/{id}")
     public R<String> getPrescription(@PathVariable String id, @PathVariable String resource) {
 
